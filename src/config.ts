f0cbe61e61fba6146d45b929019ef0e6803b90b2
Mridul <mridulyadav3173@gmail.com>
2025-05-10 22:57:15 +0530

    fixed env imports

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ dotenv.config();
 export const ENCRYPTION_KEY =
   process.env.ENCRYPTION_KEY || "12345678901234567890123456789012"; // 32 chars
 export const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/walletdb";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/walletdb";
 export const JWT_SECRET = process.env.JWT_SECRET || '1234';
